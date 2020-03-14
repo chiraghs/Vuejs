b2f601442ded3e50946277862fb1cf9b69d056c5
chiraghs <chiraghs1929@gmail.com>
2020-03-14 22:48:38 +0530

    Events Change Image on Selecting color

--- products_frontview/main.js
+++ products_frontview/main.js
@@ -5,7 +5,20 @@ var app =new Vue({
         image:'./images/green.jpg',
         decimg:'image_of_product',
         instock:-1,
+        cart:0,
         details:['85% cotton','winter wear','ankle fit'],
-        variants:[{variantID:2234,variantColor:"Green"},{variantID:2235,variantColor:"Blue"}]
+        variants:[
+        {variantID:2234,variantColor:"Green",variantImage:"./images/green.jpg"},
+        {variantID:2235,variantColor:"Blue",variantImage:"./images/blue.jpg"}
+                 ]
+    },
+    methods:{
+        addtocart(){
+            this.cart += 1;
+        },
+        updateImage(variantImage){
+            this.image=variantImage;
+        }
+
     }
 })
