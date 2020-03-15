ed11d549f41085b170761e230591b05e21dcd5a9
chiraghs <chiraghs1929@gmail.com>
2020-03-15 06:39:16 +0530

    Products with computed properties

--- products_frontview - Components/main.js
+++ products_frontview - Components/main.js
@@ -1,78 +1,104 @@
-Vue.component('product',{
-    template:`
-    <div class="product">
-
-
-    <div class="product-image">
-        <img v-bind:src="image" v-bind:alt="decimg">
-    </div>
-
-
-
-    <div class="product-info">
-        <h1>{{title}}</h1>
-        <p v-if='instock > 10'>In Stock</p>
-        <p v-else-if='instock <10 && instock  > 0'>Hurry, Almost Sold Out!!</p>
-        <p v-else>out of Stock</p>
-
-        <ul>
-            <li v-for="detail in details">{{detail}}</li>
-        </ul>
-
-        <div v-for="variant in variants" 
-             :key="variant.variantID"
-             class="color-box"
-             :style="{backgroundColor:variant.variantColor}"
-             @mouseover="updateImage(variant.variantImage)">
-            
-        </div>
-
-        <button v-on:click="addtocart()" :disabled="instock <= 0"
-                         :class="{disabledButton: instock <=0}"
-        >Add to cart</button>
-       
-        <div class="cart">
-            <p>Cart ({{cart}})</p>
-        </div>
-
-
-    </div>
-
-
-</div>   
-    `,
-    data(){
-        return {
-            product:'socks',
-            brand:'Vue Masterful',
-            image:'./images/green.jpg',
-            decimg:'image_of_product',
-            instock:5,
-            cart:0,
-            details:['85% cotton','winter wear','ankle fit'],
-            variants:[
-            {variantID:2234,variantColor:"green",variantImage:"./images/green.jpg"},
-            {variantID:2235,variantColor:"blue",variantImage:"./images/blue.jpg"}
-                     ]
-        }
+Vue.component('product', {
+    props: {
+      premium: {
+        type: Boolean,
+        required: true
+      }
     },
-    methods:{
-        addtocart(){
-            this.cart += 1;
+    template: `
+     <div class="product">
+          
+        <div class="product-image">
+          <img :src="image" />
+        </div>
+  
+        <div class="product-info">
+            <h1>{{ product }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+  
+            <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+  
+            <div class="color-box"
+                 v-for="(variant, index) in variants" 
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 @mouseover="updateProduct(index)"
+                 >
+            </div> 
+  
+            <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+              >
+            Add to cart
+            </button>
+  
+         </div>  
+      
+      </div>
+     `,
+    data() {
+      return {
+          product: 'Socks',
+          brand: 'Vue Mastery',
+          selectedVariant: 0,
+          details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+          variants: [
+            {
+              variantId: 2234,
+              variantColor: 'green',
+              variantImage: './images/green.jpg',
+              variantQuantity: 10     
+            },
+            {
+              variantId: 2235,
+              variantColor: 'blue',
+              variantImage: './images/blue.jpg',
+              variantQuantity: 0     
+            }
+          ]
+      }
+    },
+      methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
-        updateImage(variantImage){
-            this.image=variantImage;
+        updateProduct(index) {  
+            this.selectedVariant = index
         }
-
-    },
-    computed:{
-        title(){
-           return this.brand +" "+this.product
+      },
+      computed: {
+          title() {
+              return this.brand + ' ' + this.product  
+          },
+          image(){
+              return this.variants[this.selectedVariant].variantImage
+          },
+          inStock(){
+              return this.variants[this.selectedVariant].variantQuantity
+          },
+          shipping() {
+            if (this.premium) {
+              return "Free"
+            }
+              return 50
+          }
+      }
+  })
+  
+  var app = new Vue({
+      el: '#app',
+      data: {
+        premium: true,
+        cart: []
+      },
+      methods: {
+        updateCart(id) {
+          this.cart.push(id)
         }
-    }
-})
-
-var app =new Vue({
-    el:'#app',
-   
-})
+      }
+  })
