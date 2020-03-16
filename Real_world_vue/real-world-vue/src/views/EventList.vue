84b546314bb868ee42b7a5f096e49e4662714348
chiraghs <chiraghs1929@gmail.com>
2020-03-16 05:56:06 +0530

    Event card components added

--- Real_world_vue/real-world-vue/src/views/EventList.vue
+++ Real_world_vue/real-world-vue/src/views/EventList.vue
@@ -1,6 +1,20 @@
 <template>
 <div>
     <h1>Event Lists</h1>
+    <EventCard/>
     <router-link :to="{name:'event-show',params:{id:'1'}}">Show event #1</router-link> 
 </div>    
 </template>
+
+
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+    components:{
+        EventCard
+    }
+    
+}
+</script>
