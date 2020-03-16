12a6b8964acc4fd55d262ba985e71c4f398cae82
chiraghs <chiraghs1929@gmail.com>
2020-03-16 07:51:17 +0530

    data loading from api

--- Real_world_vue/real-world-vue/src/views/EventList.vue
+++ Real_world_vue/real-world-vue/src/views/EventList.vue
@@ -1,21 +1,31 @@
-<template>
-<div>
-    <h1>Event Lists</h1>
-    <EventCard/>
-    <BaseIcon/>
-    <router-link :to="{name:'event-show',params:{id:'1'}}"></router-link> 
-</div>    
-</template>
-
-
-
-<script>
-import EventCard from '@/components/EventCard.vue'
-
-export default {
-    components:{
-        EventCard
-    }
+   <template>
+      <div>
+        <h1>Events Listing</h1>
+        <EventCard v-for="event in events" :key="event.id" :event="event"/>
+      </div>
+    </template>
+    <script>
+    import EventCard from '@/components/EventCard.vue'
+    import axios from 'axios'
     
-}
-</script>
+    export default {
+      components: {
+        EventCard
+      },
+      data() {
+        return {
+          events: []
+        }
+      },
+      created() {
+        axios
+          .get('http://localhost:3000/events') 
+          .then(response => {
+            this.events = response.data // <--- set the events data
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+      }
+    }
+    </script>
